b0a91a6c28e966cc5ace9bcde5c4ce5182bd14c3
cosmina iosifescu <cosmina.iosifescu@gmail.com>
2022-01-14 11:36:21 +0100

    setup the routes and the react

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,22 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all    
+  end
+
+  def  show
+    
+  end
+  def new
+  
+  end
+  def edit
+  
+  end
+  def update
+  
+  end
+  def destroy
+  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
 end
+
+# #verb 'path', to:'controller#action'
+# #CRUD
+# #CREATE
+# get '/tasks/new', to:'tasks#new' #display the form
+# post '/tasks', to:'tasks#create'
+# #READ
+# get '/tasks', to: 'tasks#index'
+# get '/tasks/:id', to: 'tasks#show'
+# #UPDATE
+# get '/tasks/:id/edit', to: 'tasks#edit'
+# patch '/tasks/:id', to'tasks#update'
+# #DESTROY
+#   delete '/tasks/:id', to: 'tasks#destroy'
+ 
